5f79b39b25eaf22de8d03b0b86dac1d0856d6d16
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-04-10 16:16:48 +0530

    Time: 6 ms (7.85%), Space: 7.8 MB (36.11%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int helper(vector<int> &nums,int n,vector<int> &dp){
+        if(n<0) return 0;
+        if(n==0) return nums[0];
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=max(helper(nums,n-2,dp)+nums[n] ,helper(nums,n-1,dp));
+        return dp[n];
+    }
+    int rob(vector<int>& nums) {
+      vector<int> dp(nums.size(),-1);
+      return  helper(nums,nums.size()-1,dp); 
+    }
+};
