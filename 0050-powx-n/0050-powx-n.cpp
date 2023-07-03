a374ec1de39be524ee33cbda3e64251dd6f94c8f
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-07-04 00:50:04 +0530

    Time: 2 ms (47.15%), Space: 6.1 MB (30.93%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long long nn=n;
+        if(nn<0){
+            nn*=-1;
+        }
+        while(nn){
+            if(nn%2){
+                ans*=x;
+                nn-=1;
+            }
+            else{
+                x*=x;
+                nn/=2;
+            }
+        }
+        if(n<0){
+            ans=1/ans;
+        }
+        return ans;
+    }
+};
