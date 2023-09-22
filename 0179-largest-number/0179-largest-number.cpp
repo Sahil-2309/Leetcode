b022fa90e7c4ef772587ec33d4962f5bc8e63853
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-09-23 01:46:47 +0530

    Time: 3 ms (96.06%), Space: 11.4 MB (61.46%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -3,11 +3,12 @@ public:
     static bool comp(int a,int b){
     return to_string(a)+to_string(b)>to_string(b)+to_string(a);
     }
-    string largestNumber(vector<int>& arr) {
-        sort(arr.begin(),arr.end(),comp);
+    string largestNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end(),comp);
         string ans = "";
-        for(int i = 0;i<arr.size();i++)
-        ans+=to_string(arr[i]);
+        for(int i = 0;i<n;i++)
+        ans+=to_string(nums[i]);
         if(ans[0]=='0') return "0";
         return ans; 
     }
